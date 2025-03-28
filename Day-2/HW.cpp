e07e3d6af3a3e921a4212cfe8ad11fa21c74211e
Amar pal <amarpal.i.a.s@gmail.com>
2025-03-28 05:58:56 +0530

    hw init

--- Day-2/HW.cpp
+++ Day-2/HW.cpp
@@ -38,7 +38,18 @@ int main(){
 
 //-------------------------
 
+char ch ;
 
+cout << "Enter a character: " << endl;
+cin >> ch;
+
+if(ch>='a'&& ch<='z'){
+    cout << "This is LowerCase"<< endl;
+}else if(ch>='A'&& ch<='Z'){
+    cout << "This is UpperCase" << endl;
+}else if (ch>='0'&& ch<='9'){
+    cout << "This is numeric" << endl;
+}
 
 
 
